7d4b50a21ff7ebdf7f2923c373523a983b116690
Hashim Tabassum <114857943+Hashim-tabassum-github@users.noreply.github.com>
2022-10-17 06:44:34 -0700

    para
    this is method to write apar graf

---
+++ app.js
@@ -0,0 +1,13 @@
+function getpara(){
+    var txt=" i am a student of of data science in gift univeristy gugrawala.i am a heard werking student    i am work on my project for 3 year .my dreem is want to be come a data scienctist .after one year i am going to astril.and may be germaney."
+
+    var para=document.getElementById('para');
+    para.innerHTML = txt
+}
+
+
+function copytext(){
+    var text = document.getElementById('text').innerHTML;
+    var paragraf = document.getElementById('parra')
+    paragraf.innerHTML = text
+}
